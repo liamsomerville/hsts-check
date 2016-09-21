6b64b3cea0e9184d74537848fce08d80f17df4b5
Liam Somerville <liamsomerville@gmail.com>
2016-09-21 23:47:27 +0100

    Added new features and additional checks
    As well as just checking if HSTS is enabled this version now checks for additional components
    - is HSTS includeSubDomains set?
    - is HSTS max-age defined as 2 years
    - is HSTS preload configured
    
    As well as the additional checks, the script now includes a debug mode which shows you the server response.

--- hsts.py
+++ hsts.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
-
 import sys
 import requests
 import urllib3
 import certifi
+
+#if you are having issues then set DEBUG=True
+DEBUG=False
 
 http = urllib3.PoolManager(
     cert_reqs='CERT_REQUIRED', # Force certificate check.
@@ -22,7 +24,24 @@ else:
 
 #Peform the HSTS check and display the result
 check = http.request('GET', url, headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'})
-if 'strict-transport-security' in check.headers:
- print site + ': is using HSTS!!!'
+response=check.headers
+if 'strict-transport-security' in response:
+    print "[+] " +site + ': is using HSTS!!!'
+    if 'preload' in str(response):
+        print "  [+] Preload enabled"
+    else:
+    	print "  [Warning!] Preload is not configured"    
+    if 'includeSubDomains' in str(response):
+       print "  [+] includeSubdomains is present"
+    else:
+       print "  [Warning!] includeSubDomains is not configured"
+    if 'max-age=31536000' in str(response):
+	print "  [+] max-age is set to two years - well done"
+    else:
+	print "  [Warning!] max-age should really be set to two years (31536000)"
+    if DEBUG:
+        print str(response)
 else:
- print site + ': is NOT using HSTS'
+    print site + ': is NOT using HSTS'
+    if DEBUG:
+        print str(response)
